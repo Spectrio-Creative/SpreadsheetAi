26b9bd5fe365fdaef444a2234b66c26711f8de44
Adam Howard <adam.howard.music@gmail.com>
2024-01-23 13:36:22 +0100

    Move color tools

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -76,22 +76,3 @@ export function stringToObj(str: string) {
   return JSON.parse(jsonStr);
 }
 
-export function hexToRgb(hex: string) {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16),
-    }
-    : null;
-}
-
-export function componentToHex(c) {
-  const hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-export function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
