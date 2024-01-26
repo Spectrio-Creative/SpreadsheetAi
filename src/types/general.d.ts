9d72eb363437f4a09c946d5f1732de486da4ae6a
Adam Howard <adam.howard.music@gmail.com>
2024-01-26 15:03:17 +0100

    Extend types to avoid ts errors

--- src/types/general.d.ts
+++ src/types/general.d.ts
@@ -11,4 +11,38 @@ interface SpreadsheetRow extends BasicObject {
     "Layer Name": string;
 }
 
-type dimension = 'width' | 'height';
+type DimensionType = 'width' | 'height';
+
+type SizeOption = "contain" | "cover" | "original";
+
+type HorizontalAlignment = "left" | "center" | "right";
+
+type VerticalAlignment = "top" | "center" | "bottom";
+
+type Alignment = HorizontalAlignment | VerticalAlignment;
+
+type DoubleAlignment = "left top" | "left center" | "left bottom" | "right top" | "right center" | "right bottom" | "center top" | "center center" | "center bottom";
+
+// interface PageItemUUID extends PageItem {
+//     uuid?: string;
+// }
+
+interface PageItem {
+    uuid?: string;
+}
+
+interface TextRange {
+    start: number;
+    end: number;
+}
+
+declare const enum ElementPlacement {
+    // @ts-ignore
+    PLACEATBEGINNING = ElementPlacement.PLACEATBEGINNING,
+    // @ts-ignore
+    PLACEATEND = ElementPlacement.PLACEATEND,
+    // @ts-ignore
+    PLACEBEFORE = ElementPlacement.PLACEBEFORE,
+    // @ts-ignore
+    PLACEAFTER = ElementPlacement.PLACEAFTER,
+}
