08706e8cb1f57279ef3197fa38ccf33705383633
Adam Howard <adam.howard.music@gmail.com>
2024-01-26 15:02:45 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   ignorePatterns: ["package.json", "actions/*", "build/*", "dist/*"],
   rules: {
-    indent: ["error", 2],
+    indent: ["error", 2, { "SwitchCase": 1 }],
     "linebreak-style": ["error", "unix"],
     // quotes: ["error", "double"],
     semi: ["error", "always"],
