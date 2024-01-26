4bf2e4825328464bb0023e93989795520a8f7bc5
Adam Howard <adam.howard.music@gmail.com>
2024-01-26 15:20:40 +0100

    Update classes.ts

--- src/tools/classes.ts
+++ src/tools/classes.ts
@@ -1,10 +1,50 @@
-import { layer_options } from "./regExTests";
+import camelCase from 'just-camel-case';
+import { AiGroupItem } from '../classes/AiGroupItem';
+import { AiPageItem } from '../classes/AiPageItem';
+import { AiTextBox } from '../classes/AiTextBox';
+import { aiObjByUUID, layer_sheet_cc } from '../globals/globals';
+import { is_color, is_image, layer_options } from "./regExTests";
 import { stringToObj } from "./tools";
+import { AiImage } from '../classes/AiImage';
+import { AiColorShape } from '../classes/AiColorShape';
 
 export const locationsAsSingleString = /^([\w.]+) ([\w.]+)/;
 
 export const isStringLocation = (location: string) => {
   return locationsAsSingleString.test(location);
+};
+
+export const parseAlignment = (x: Alignment | DoubleAlignment, y?: Alignment): [HorizontalAlignment, VerticalAlignment] => {
+  // If x is DoubleAlignment, separate into x and y
+  if (typeof x === "string" && / /.test(x)) {
+    let [xAlign, yAlign] = x.split(" ");
+
+    // Check to make sure the values aren't reversed and fix if they are
+    if (
+      (/^(top|bottom)$/.test(xAlign) && /^(left|right|center)$/.test(yAlign)) ||
+      (/^(left|right)$/.test(yAlign) && /^(top|bottom|center)$/.test(xAlign))
+    ) {
+      const temp = xAlign;
+      xAlign = yAlign;
+      yAlign = temp;
+    }
+
+    x = xAlign as HorizontalAlignment;
+    y = yAlign as VerticalAlignment;
+  }
+
+  if (/^(top|bottom)$/.test(x)) {
+    y = x as VerticalAlignment;
+    x = "center";
+  }
+
+  y = y || (x === "center" ? "center" : "top");
+
+  // Make sure x and y are valid values
+  x = (/^(left|center|right)$/.test(x) ? x : "center") as HorizontalAlignment;
+  y = (/^(top|center|bottom)$/.test(y) ? y : "center") as VerticalAlignment;
+
+  return [x, y];
 };
 
 export const parseLocation = (location: string) => {
@@ -28,7 +68,9 @@ export const parseLocation = (location: string) => {
   return { x: parseFloat(val1), y: parseFloat(val2) };
 };
 
-export const getGroupAlignment = (group: GroupItem) => {
+export const getGroupAlignment = (
+  group: GroupItem
+): { x: HorizontalAlignment; y: VerticalAlignment } => {
   if (group.pageItems.length > 2) return { x: "center", y: "center" };
   let textAlignment = "center";
 
@@ -38,7 +80,9 @@ export const getGroupAlignment = (group: GroupItem) => {
       : {};
     $.write(options);
     if (item.typename === "TextFrame")
-      textAlignment = (item as TextFrame).textRange.paragraphAttributes.justification
+      textAlignment = (
+        item as TextFrame
+      ).textRange.paragraphAttributes.justification
         .toString()
         .toLowerCase()
         .replace("justification.", "")
@@ -46,7 +90,7 @@ export const getGroupAlignment = (group: GroupItem) => {
   });
 
   const groupAlignment = /^(left|right|center)$/.test(textAlignment)
-    ? textAlignment
+    ? (textAlignment as HorizontalAlignment)
     : "center";
 
   return { x: groupAlignment, y: "center" };
@@ -54,4 +98,65 @@ export const getGroupAlignment = (group: GroupItem) => {
 
 export const getFontFamily = (textFont: TextFont) => {
   return app.textFonts.filter((tf) => tf.family === textFont.family);
+};
+
+export const calculatePosition = (model: Box, obj: Box) => {
+  const top = () => model.top;
+  const left = () => model.left;
+
+  const bottom = () => model.top - (model.height - obj.height);
+  const right = () => model.left + (model.width - obj.width);
+
+  const center = () => (model.width - obj.width) / 2 + model.left;
+  const middle = () => (-1 * (model.height - obj.height)) / 2 + model.top;
+
+  const output: { [key in DoubleAlignment]: () => [number, number] } = {
+    "left top": () => [left(), top()],
+    "left center": () => [left(), middle()],
+    "left bottom": () => [left(), bottom()],
+    "center top": () => [center(), top()],
+    "center center": () => [center(), middle()],
+    "center bottom": () => [center(), bottom()],
+    "right top": () => [right(), top()],
+    "right center": () => [right(), middle()],
+    "right bottom": () => [right(), bottom()],
+  };
+
+  return output;
+};
+
+export const parseOptions = (layerName: string) => {
+  const options = layer_options.test(layerName)
+    ? stringToObj(layerName.match(layer_options)[1])
+    : {};
+
+  return options;
+};
+
+export type ItemClassType = "AiPageItem" | "AiGroupItem" | "AiImage" | "AiColorShape" | "AiTextBox";
+
+export const getOrMakeItemClass = (item: PageItem, type: ItemClassType | "infer" = 'infer'): AiPageItem => {
+  const uuid = item.uuid;
+  if (uuid && aiObjByUUID[uuid]) return aiObjByUUID[uuid];
+
+  const newValue = layer_sheet_cc[camelCase(item.name.replace(/\s*\{.*\}\s*$/g, ""))];
+
+  if (type === "infer") {
+    if (is_image.test(newValue)) return getOrMakeItemClass(item, "AiImage");
+    if (is_color.test(newValue)) return getOrMakeItemClass(item, "AiColorShape");
+    if (item.typename === "TextFrame") return getOrMakeItemClass(item, "AiTextBox");
+    if (item.typename === "GroupItem") return getOrMakeItemClass(item, "AiGroupItem");
+    return getOrMakeItemClass(item, "AiPageItem");
+  }
+
+  if (type === "AiTextBox") return new AiTextBox(item as TextFrame, newValue);
+  if (type === "AiImage") return new AiImage(item, newValue);
+  if (type === "AiColorShape") return new AiColorShape(item as PathItem, newValue);
+  if (type === "AiGroupItem") return new AiGroupItem(item as GroupItem);
+  return new AiPageItem(item);
+};
+
+export const addItemClassToGlobal = (item: AiPageItem, uuid?: string) => {
+  if (uuid) aiObjByUUID[uuid] = item;
+  if (item.uuid) aiObjByUUID[item.uuid] = item;
 };
